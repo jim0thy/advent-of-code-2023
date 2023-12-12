5bd838cdecab976b35aec66e019805e75b5f858a
Jim Gillett <503909+jim0thy@users.noreply.github.com>
2023-12-12 23:37:53 +0000

    Add input data reading function to part2.ts
    The newly added part2.ts script now contains a function to read and process data from 'inputs/day1.txt'. The function sums up the first and last digits of each non-empty lines within the data. The final sum is displayed in the console.

---
+++ day1/part2.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from 'node:fs'
+import { join as joinPath } from 'node:path'
+
+let sum = 0
+const data = readFileSync('inputs/day1.txt', 'utf8')
+const lines: string[] = data.split('\n')
+const numbers: Record<string, string> = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+}
+
+lines.filter(line => line.length > 0).forEach((line: string) => {
+  const digits = []
+  let str = ""
+  for (let char of line) {
+    if (parseInt(char)) {
+      digits.push(char)
+    } else {
+      str += char
+      for (let num of Object.keys(numbers)) {
+        if (str.endsWith(num)) {
+          digits.push(numbers[num])
+        }
+      }
+    }
+  }
+
+  sum += parseInt(`${digits[0]}${digits[digits.length - 1]}`)
+})
+console.log(sum)
