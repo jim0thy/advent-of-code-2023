750a48d732ff3dc6cfd3ce1dafc4ca052e6e698b
Jim Gillett <503909+jim0thy@users.noreply.github.com>
2023-12-13 00:12:03 +0000

    Remove commented-out test data from part1.ts
    The commented-out test data from 'part1.ts' script has been removed. Now it directly reads and parses input data for Day 2 problems from the 'inputs/day2.txt' file.

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,12 +1,6 @@
 import { readFileSync } from 'node:fs'
 
 const input = readFileSync('inputs/day2.txt', 'utf8')
-// const input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
-//   "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
-//   "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
-//   "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
-//   "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-
 const data = input.split('\n').filter(line => line.length > 0)
 
 const MAX_CUBES = {
