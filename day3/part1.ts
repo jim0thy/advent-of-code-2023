e6a96318b76e76e036b420b476743af04a2caf4b
Jim Gillett <503909+jim0thy@users.noreply.github.com>
2023-12-15 17:58:51 +0000

    Implement `part1.ts` logic for 'day3' of AdventOfCode
    The `part1.ts` file for the third day of AdventOfCode challenge has been implemented. It includes support for reading the input from a text file, processing the input data, and returning the final sum result. It also contains various helper functions for data processing.

---
+++ day3/part1.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from 'node:fs'
+
+let sum = 0
+const data = readFileSync('inputs/day3.txt', 'utf8')
+// const data =
+//   '467..114..\n' +
+//   '...*......\n' +
+//   '..35..633.\n' +
+//   '......#...\n' +
+//   '617*......\n' +
+//   '.....+.58.\n' +
+//   '..592.....\n' +
+//   '......755.\n' +
+//   '...$.*....\n' +
+//   '.664.598..'
+
+const lines: string[][] = data.split('\n').map((line) => line.split('')).filter(line => line.length > 0)
+
+const isNumber = (n: string): boolean => !Number.isNaN(parseInt(n))
+const isAFullStop = (n: string): boolean => n === '.'
+const isASymbol = (n: string): boolean => !isNumber(n) && !isAFullStop(n)
+
+const findEntireNumber = (line: string[], index: number): number[] => {
+  let number = ''
+  let i = index
+  while (isNumber(line[i])) {
+    number += line[i]
+    i++
+  }
+  return [number.length, parseInt(number)]
+}
+
+const findBoundingChars = (j: number, length: number, i: number, line: string[]) => {
+  const start = j === 0 ? 0 : j - 1
+  const end = j === 0 ? length + 1 : j + length + 1
+  const top = lines[i - 1]?.slice(start, end) ?? []
+  const bottom = lines[i + 1]?.slice(start, end) ?? []
+  const left = line[start] ?? '.'
+  const right = line[j + length] ?? '.'
+  return { top, bottom, left, right }
+}
+
+for (const [i, line] of lines.entries()) {
+  for (let j = 0; j < line.length; j++) {
+    if (isNumber(line[j])) {
+      const [length, num] = findEntireNumber(line, j)
+      const { top, bottom, left, right } = findBoundingChars(j, length, i, line)
+      if (
+        top.some(c => isASymbol(c)) ||
+        bottom.some(c => isASymbol(c)) ||
+        isASymbol(left) ||
+        isASymbol(right)
+      ) {
+        sum += num
+      }
+      j += length
+    }
+  }
+}
+
+console.log(sum)
